2050a3804a98442dd817e3758d3f6b06cf878cfe
Meet Makwana <meetmakwana203@gmail.com>
2024-09-14 19:47:42 +0530

    feat: added the state interface

---
+++ src/components/Editor.ts
@@ -0,0 +1,12 @@
+export interface IState {
+  image: string | ArrayBuffer | null;
+  brightness: number;
+  grayscale: number;
+  sepia: number;
+  saturate: number;
+  contrast: number;
+  hueRotate: number;
+  rotate: number;
+  vartical: number;
+  horizental: number;
+}
