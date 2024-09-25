d6774250ebb48df3371130c565b4a0d882306108
Meet Makwana <meetmakwana203@gmail.com>
2024-09-25 15:48:47 +0530

    Revert "chore: downgraded to react 16"
    This reverts commit 82e88feadda776219f8270b27674429b9b2a83c8.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root')
 )
